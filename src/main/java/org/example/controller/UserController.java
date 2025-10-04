07b32fe18e39d313bf5c4240c959b2ffc7a4307f
Simun-12 <simunsarangi0912@gmail.com>
2025-10-04 22:02:28 +0530

    Swagger Configured

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,10 +1,13 @@
 package org.example.controller;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.model.User;
 import org.example.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "User API", description = "Operations related to users")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -14,11 +17,13 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
+    @Operation(summary = "Get all users")
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @Operation(summary = "Create User")
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
